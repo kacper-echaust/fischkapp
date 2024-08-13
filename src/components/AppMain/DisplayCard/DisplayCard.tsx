6514f48b152aa384cd217b307f695b2398b75cb3
kacper-echaust <k9echaust@gmail.com>
2024-08-13 17:25:50 +0200

    try to edit card

--- src/components/AppMain/DisplayCard/DisplayCard.tsx
+++ src/components/AppMain/DisplayCard/DisplayCard.tsx
@@ -1,15 +1,51 @@
-import React from "react";
-import styles from "./DisplayCard.module.css";
+import React, { ChangeEvent, useContext, useState } from 'react'
+import styles from './DisplayCard.module.css'
+import { Buttons } from '../NewCard/Buttons/Buttons'
+import { Card } from '../../types'
+import { CardContext, CardSide } from '../../Context/CardListProvider'
 
 type DisplayCardProps = {
-  value: string;
-};
+	value: Card
+}
+export enum Edit {
+	True = 'yes',
+	False = 'no',
+}
 const DisplayCard = ({ value }: DisplayCardProps) => {
-  return (
-    <div className={styles.container}>
-      <p>{value}</p>
-    </div>
-  );
-};
+	const { cardList, setCardList, currentSide } = useContext(CardContext)
+	const [isEdit, setIsEdit] = useState(Edit.False)
+	const [currentValue, setCurrentValue] = useState()
+	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		// setCurrentValue(event.target.value)
+	}
+	const handleSave = () => {
+    cardList.map(card => {
+      if(card === value) {
 
-export { DisplayCard };
+      }
+    })
+  }
+	const handleCancel = () => {
+		setIsEdit(Edit.False)
+	}
+	const handleEdit = () => {
+		setIsEdit(Edit.True)
+	}
+	return (
+		<div className={styles.container}>
+			{isEdit === Edit.True ? (
+				<>
+					<input type='text' value={currentValue} onChange={handleInputChange} />
+					<Buttons onSave={handleSave} onCancel={handleCancel} edit={isEdit} />
+				</>
+			) : (
+				<>
+					<p>{value.title}</p>
+					<img src='edit-icon.png' alt='edit icon' className={styles.editIcon} onClick={handleEdit} />
+				</>
+			)}
+		</div>
+	)
+}
+
+export { DisplayCard }
--- src/components/AppMain/NewCard/NewCard.tsx
+++ src/components/AppMain/NewCard/NewCard.tsx
@@ -1,78 +1,67 @@
-import { CardContext } from "../../Context/CardListProvider";
-import { Card } from "../../types";
-import styles from "./NewCard.module.css";
-import React, { ChangeEvent, useContext, useState } from "react";
-
-enum CardSide {
-  Front = "front",
-  Back = "back",
-}
+import { CardContext, CardSide } from '../../Context/CardListProvider'
+import { Card } from '../../types'
+import { Buttons } from './Buttons/Buttons'
+import styles from './NewCard.module.css'
+import React, { ChangeEvent, useContext, useState } from 'react'
 
 const NewCard = () => {
-  const [card, setCardValues] = useState<Card>({
-    title: "",
-    value: "",
-  });
-  const [currentSide, setCurrentSide] = useState(CardSide.Front);
-  const { setIsAddingNewCard, setCardList } = useContext(CardContext);
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (currentSide === CardSide.Front) {
-      setCardValues((prevValue) => {
-        return { ...prevValue, title: event.target.value };
-      });
-    } else {
-      setCardValues((prevValue) => {
-        return { ...prevValue, value: event.target.value };
-      });
-    }
-  };
-  const handleCancel = () => {
-    setIsAddingNewCard(false);
-  };
-  const handleNext = () => {
-    setCurrentSide(CardSide.Back);
-  };
-  const handleBack = () => {
-    setCurrentSide(CardSide.Front);
-  };
-  const handleSave = () => {
-    setCardList((prevCardList) => {
-      return [...prevCardList, card];
-    });
-    setCurrentSide(CardSide.Front);
-    setIsAddingNewCard(false);
-    setCardValues({ title: "", value: "" });
-  };
-  return (
-    <form
-      className={styles.form}
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
-      {currentSide && <label className={styles.label}>{card.title}</label>}
-      <input
-        className={styles.input}
-        type="text"
-        onChange={handleInputChange}
-        value={currentSide === CardSide.Back ? card.value : card.title}
-      />
-      <div className={styles.containerButtons}>
-        <button
-          className={styles.cancelButton}
-          onClick={currentSide === CardSide.Back ? handleBack : handleCancel}
-        >
-          {currentSide === CardSide.Back ? "Back" : "Cancel"}
-        </button>
-        <button
-          className={styles.nextButton}
-          onClick={currentSide === CardSide.Back ? handleSave : handleNext}
-        >
-          {currentSide === CardSide.Back ? "Save" : "Next"}
-        </button>
-      </div>
-    </form>
-  );
-};
+	const { setIsAddingNewCard, setCardList, currentSide, setCurrentSide } = useContext(CardContext)
+	const [card, setCardValues] = useState<Card>({
+		title: '',
+		value: '',
+	})
 
-export { NewCard };
+	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (currentSide === CardSide.Front) {
+			setCardValues(prevValue => {
+				return { ...prevValue, title: event.target.value }
+			})
+		} else {
+			setCardValues(prevValue => {
+				return { ...prevValue, value: event.target.value }
+			})
+		}
+	}
+	const handleCancel = () => {
+		setIsAddingNewCard(false)
+	}
+	const handleNext = () => {
+		setCurrentSide(CardSide.Back)
+	}
+	const handleBack = () => {
+		setCurrentSide(CardSide.Front)
+	}
+	const handleSave = () => {
+		setCardList(prevCardList => {
+			return [...prevCardList, card]
+		})
+		setCurrentSide(CardSide.Front)
+		setIsAddingNewCard(false)
+		setCardValues({ title: '', value: '' })
+	}
+	return (
+		<form
+			className={styles.form}
+			onSubmit={event => {
+				event.preventDefault()
+			}}>
+			{currentSide && <label className={styles.label}>{card.title}</label>}
+			<input
+				className={styles.input}
+				type='text'
+				onChange={handleInputChange}
+				value={currentSide === CardSide.Back ? card.value : card.title}
+			/>
+			<Buttons
+				side={currentSide}
+				onCancel={handleCancel}
+				onNext={handleNext}
+				onBack={handleBack}
+				onSave={handleSave}
+				edit={''}
+			/>
+		</form>
+	)
+}
+
+export { NewCard, CardSide }
