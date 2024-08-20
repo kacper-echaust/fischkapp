98d97016f11a89dc2d7b72ae9ef33ce3c6381224
kacper-echaust <k9echaust@gmail.com>
2024-08-20 08:35:21 +0200

    add loading and change error

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -3,6 +3,7 @@ import { Card } from './components/types'
 
 const useFetch = () => {
 	const [dataFromApi, setDataFromApi] = useState<Card[]>([])
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		fetch('https://training.nerdbord.io/api/v1/fischkapp/flashcards', {
@@ -16,10 +17,16 @@ const useFetch = () => {
 					throw new Error()
 				}
 			})
-			.then(data => setDataFromApi(data))
-			.catch(error => console.log(`Error: ${error}`))
+			.then(data => {
+				setDataFromApi(data)
+				setIsLoading(false)
+			})
+			.catch(error => {
+				setIsLoading(false)
+				console.error(`Failed to fetch flashcards: ${error}`)
+			})
 	}, [])
-	return { dataFromApi,setDataFromApi }
+	return { dataFromApi, setDataFromApi, isLoading }
 }
 
 export { useFetch }
