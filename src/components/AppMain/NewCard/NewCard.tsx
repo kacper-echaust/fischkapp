e1094879c8ab677bca06fc2ff21533f4d32278aa
kacper-echaust <k9echaust@gmail.com>
2024-08-21 07:40:23 +0200

    create fetch with method post

--- src/components/AppMain/NewCard/NewCard.tsx
+++ src/components/AppMain/NewCard/NewCard.tsx
@@ -1,83 +1,77 @@
-import { CardContext, CardSide } from "../../Context/CardListProvider";
-import { Card } from "../../types";
-import { Buttons } from "./Buttons/Buttons";
-import styles from "./NewCard.module.css";
-import React, { ChangeEvent, useContext, useState } from "react";
+import { useFetch } from '../../../useFetch'
+import { CardContext, CardSide } from '../../Context/CardListProvider'
+import { Buttons } from './Buttons/Buttons'
+import styles from './NewCard.module.css'
+import React, { ChangeEvent, useContext, useState } from 'react'
 
 const NewCard = () => {
-  const { setIsAddingNewCard, setCardList, currentSide, setCurrentSide } =
-    useContext(CardContext);
-  const [card, setCardValues] = useState<Card>({
-    title: "",
-    value: "",
-    id: 0,
-  });
+	const { setIsAddingNewCard, setCardList, currentSide, setCurrentSide } = useContext(CardContext)
+	const { addNewCard } = useFetch()
+	const [card, setCardValues] = useState({
+		front: '',
+		back: '',
+	})
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (currentSide === CardSide.Front) {
-      setCardValues((prevValue) => {
-        return { ...prevValue, title: event.target.value, id: Date.now() };
-      });
-    } else {
-      setCardValues((prevValue) => {
-        return { ...prevValue, value: event.target.value, id: Date.now() };
-      });
-    }
-  };
-  const handleCancel = () => {
-    setIsAddingNewCard(false);
-  };
-  const handleNext = () => {
-    setCurrentSide(CardSide.Back);
-  };
-  const handleBack = () => {
-    setCurrentSide(CardSide.Front);
-  };
-  const handleSave = () => {
-    setCardList((prevCardList) => {
-      return [...prevCardList, card];
-    });
-    setCurrentSide(CardSide.Front);
-    setIsAddingNewCard(false);
-    setCardValues({ title: "", value: "", id: 0 });
-  };
-  const handleDelete = () => {
-    setCardValues({ title: "", value: "", id: 0 });
-    setCurrentSide(CardSide.Front);
-    setIsAddingNewCard(false);
-  };
-  return (
-    <form
-      className={styles.form}
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
-      {currentSide && <label className={styles.label}>{card.title}</label>}
-      <input
-        className={styles.input}
-        type="text"
-        onChange={handleInputChange}
-        value={currentSide === CardSide.Back ? card.value : card.title}
-      />
-      <Buttons
-        side={currentSide}
-        onCancel={handleCancel}
-        onNext={handleNext}
-        onBack={handleBack}
-        onSave={handleSave}
-        edit={""}
-      />
-      {currentSide === CardSide.Back && (
-        <img
-          src="trash-icon.png"
-          alt="trash icon"
-          className={styles.editIcon}
-          onClick={handleDelete}
-        />
-      )}
-    </form>
-  );
-};
+	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (currentSide === CardSide.Front) {
+			setCardValues(prevValue => {
+				return { ...prevValue, front: event.target.value, id: Date.now() }
+			})
+		} else {
+			setCardValues(prevValue => {
+				return { ...prevValue, back: event.target.value, id: Date.now() }
+			})
+		}
+	}
+	const handleCancel = () => {
+		setIsAddingNewCard(false)
+	}
+	const handleNext = () => {
+		setCurrentSide(CardSide.Back)
+	}
+	const handleBack = () => {
+		setCurrentSide(CardSide.Front)
+	}
+	const handleSave = () => {
+		// setCardList((prevCardList) => {
+		//   return [...prevCardList, card];
+		// });
+		addNewCard(card.front, card.back)
+		setCurrentSide(CardSide.Front)
+		setIsAddingNewCard(false)
+		setCardValues({ front: '', back: '' })
+	}
+	const handleDelete = () => {
+		setCardValues({ front: '', back: '' })
+		setCurrentSide(CardSide.Front)
+		setIsAddingNewCard(false)
+	}
+	return (
+		<form
+			className={styles.form}
+			onSubmit={event => {
+				event.preventDefault()
+			}}>
+			{currentSide && <label className={styles.label}>{card.front}</label>}
+			<input
+				className={styles.input}
+				type='text'
+				onChange={handleInputChange}
+				value={currentSide === CardSide.Back ? card.back : card.front}
+			/>
+			<Buttons
+				side={currentSide}
+				onCancel={handleCancel}
+				onNext={handleNext}
+				onBack={handleBack}
+				onSave={handleSave}
+				edit={false}
+			/>
+			{currentSide === CardSide.Back && (
+				<img src='trash-icon.png' alt='trash icon' className={styles.editIcon} onClick={handleDelete} />
+			)}
+		</form>
+	)
+}
 
-export { NewCard, CardSide };
+export { NewCard, CardSide }
--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
 import { Card } from './components/types'
 
+const URL = 'https://training.nerdbord.io/api/v1/fischkapp/flashcards'
 const useFetch = () => {
 	const [dataFromApi, setDataFromApi] = useState<Card[]>([])
 	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
-		fetch('https://training.nerdbord.io/api/v1/fischkapp/flashcards', {
+		fetch(URL, {
 			method: 'GET',
 		})
 			.then(res => {
@@ -26,7 +27,32 @@ const useFetch = () => {
 				console.error(`Failed to fetch flashcards: ${error}`)
 			})
 	}, [])
-	return { dataFromApi, setDataFromApi, isLoading }
+	const addNewCard = (front: string, back: string) => {
+		fetch(URL, {
+			method: 'POST',
+			headers: {
+				Authorization: 'secret-token',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				front: { front },
+				back: { back },
+			}),
+		})
+			.then(res => {
+				if (res.ok) {
+					return res.json()
+				}
+				throw new Error('Network response was not ok')
+			})
+			.then(data => {
+				console.log(`Succes`, data)
+			})
+			.catch(error => {
+				console.error('Error', error)
+			})
+	}
+	return { dataFromApi, setDataFromApi, isLoading, addNewCard }
 }
 
 export { useFetch }
