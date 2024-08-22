eb30a2b0628c597a4c88ddcb2c4d6d64a7a81436
kacper-echaust <k9echaust@gmail.com>
2024-08-22 07:32:23 +0200

    code shortening in useFetch

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -1,121 +1,103 @@
-import { useEffect, useState } from "react";
-import { Card } from "./components/types";
-
-const URL = "https://training.nerdbord.io/api/v1/fischkapp/flashcards";
+import { useEffect, useState } from 'react'
+import { Card } from './components/types'
+enum Methods {
+	Post = 'POST',
+	Patch = 'PATCH',
+	Delete = 'DELETE',
+}
+const URL = 'https://training.nerdbord.io/api/v1/fischkapp/flashcards'
 const useFetch = () => {
-  const [dataFromApi, setDataFromApi] = useState<Card[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+	const [dataFromApi, setDataFromApi] = useState<Card[]>([])
+	const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(URL, {
-      method: "GET",
-    })
-      .then((res) => {
-        if (res.ok) {
-          console.log("Succes");
-          return res.json();
-        } else {
-          throw new Error();
-        }
-      })
-      .then((data) => {
-        setDataFromApi(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        console.error(`Failed to fetch flashcards: ${error}`);
-      });
-  }, []);
-  const addNewCard = (front: string, back: string) => {
-    setIsLoading(true);
-    fetch(URL, {
-      method: "POST",
-      headers: {
-        Authorization: "secret_token",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        front: front,
-        back: back,
-      }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Network response was not ok");
-      })
-      .then((data) => {
-        console.log(`Succes`, data);
-        setDataFromApi((prevData) => {
-          return [...prevData, data];
-        });
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
-  };
-  const editCard = (id: string, front: string, back: string) => {
-    setIsLoading(true);
-    fetch(`${URL}/${id}`, {
-      method: "PATCH",
-      headers: {
-        Authorization: "secret_token",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        front: front,
-        back: back,
-      }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Network response was not ok");
-      })
-      .then((data) => {
-        console.log(`Succes`, data);
-        setDataFromApi((prevData) => {
-          return [...prevData, data];
-        });
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
-  };
-  const deleteCard = (id:string) => {
-    setIsLoading(true);
-    fetch(`${URL}/${id}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: "secret_token"
-      },
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Network response was not ok");
-      })
-      .then((data) => {
-        console.log(`Succes`, data);
-        setDataFromApi((prevData) => {
-          return prevData.filter(() => {
-            id !== prevData['_id']
-          })
-        });
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
-  }
-  return { dataFromApi, setDataFromApi, isLoading, addNewCard, editCard,deleteCard };
-};
+	useEffect(() => {
+		setIsLoading(true)
+		fetch(URL, {
+			method: 'GET',
+		})
+			.then(res => {
+				if (res.ok) {
+					console.log('Succes')
+					return res.json()
+				} else {
+					throw new Error()
+				}
+			})
+			.then(data => {
+				setDataFromApi(data)
+				setIsLoading(false)
+			})
+			.catch(error => {
+				setIsLoading(false)
+				console.error(`Failed to fetch flashcards: ${error}`)
+			})
+	}, [])
+	const addNewCard = async (front: string, back: string) => {
+		const options = {
+			headers: {
+				Authorization: 'secret_token',
+			},
+			body: JSON.stringify({ front: front, back: back }),
+		}
+		await fetchData(Methods.Post, URL, options)
+	}
+	const editCard = async (id: string, front: string, back: string) => {
+		const options = {
+			headers: {
+				Authorization: 'secret_token',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				front: front,
+				back: back,
+			}),
+		}
+		await fetchData(Methods.Patch, `${URL}/${id}`, options)
+	}
+	const deleteCard = async (id: string) => {
+		const options = {
+			headers: {
+				Authorization: 'secret_token',
+			},
+		}
+		await fetchData(Methods.Delete, `${URL}/${id}`, options)
+		setDataFromApi(prevData => {
+			return prevData.filter(() => {
+				id !== prevData['_id']
+			})
+		})
+	}
+	const fetchData = (
+		method = Methods.Post,
+		URL: string,
+		options: {
+			headers: {
+				Authorization: string
+			}
+			body?: string
+		}
+	) => {
+		setIsLoading(true)
+		fetch(URL, {
+			method: method,
+			...options,
+		})
+			.then(res => {
+				if (res.ok) {
+					return res.json()
+				}
+				throw new Error('Network response was not ok')
+			})
+			.then(data => {
+				console.log(`Succes`, data)
+				setDataFromApi(data)
+				setIsLoading(false)
+			})
+			.catch(error => {
+				console.error('Error', error)
+			})
+	}
+	return { dataFromApi, setDataFromApi, isLoading, addNewCard, editCard, deleteCard }
+}
 
-export { useFetch };
+export { useFetch }
