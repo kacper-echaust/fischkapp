ae2a7536d59af37afd4a100f5422b90c6ace4cc6
kacper-echaust <k9echaust@gmail.com>
2024-09-02 19:48:45 +0200

    test delete card

--- src/tests/DisplayCard.test.tsx
+++ src/tests/DisplayCard.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { DisplayCard } from '../components/AppMain/DisplayCard/DisplayCard'
 
-test('edit card', () => {
+test('try to edit and delete card', () => {
 	render(<DisplayCard front='front' back='back' _id={'123'} key={'123'} />)
 
 	const editIcon = screen.getByAltText('edit icon')
@@ -13,6 +13,7 @@ test('edit card', () => {
 	const input = screen.getByRole('textbox') as HTMLInputElement
 	const saveButton = screen.getByRole('button', { name: /save/i })
 	const cancelButton = screen.getByRole('button', { name: /cancel/i })
+	
 
 	fireEvent.change(input, { target: { value: '' } })
 	expect(input.value).toBe('')
@@ -29,4 +30,14 @@ test('edit card', () => {
 	const text = screen.getByRole('paragraph') as HTMLParagraphElement
 
 	expect(text.textContent).toBe('something')
+
+	const flashCard = screen.getByText('something')
+
+	expect(flashCard).toBeInTheDocument()
+	fireEvent.click(editIcon)
+
+	const deleteIcon = screen.getByAltText('trash icon')
+	fireEvent.click(deleteIcon)
+
+	expect(flashCard).not.toBeInTheDocument()
 })
