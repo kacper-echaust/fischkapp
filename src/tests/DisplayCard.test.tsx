69b8d23d6df8c06a168ebadf2307a338ffa56651
kacper-echaust <k9echaust@gmail.com>
2024-09-03 19:55:08 +0200

    correct edit and delete card tests

--- src/tests/DisplayCard.test.tsx
+++ src/tests/DisplayCard.test.tsx
@@ -1,58 +1,37 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import { DisplayCard } from '../components/AppMain/DisplayCard/DisplayCard'
+import { AppMain } from '../components/AppMain/AppMain'
 
-test('try to edit and delete card', () => {
-	render(<DisplayCard front='front' back='back' _id={'123'} key={'123'} />)
+test('edit card', () => {
+	render(<AppMain />)
 
-	const editIcon = screen.getByAltText('edit icon')
-
+	const editIcon = screen.getAllByAltText('edit icon')[0]
 	fireEvent.click(editIcon)
 
-	const input = screen.getByRole('textbox') as HTMLInputElement
-	const saveButton = screen.getByRole('button', { name: /save/i })
-	const cancelButton = screen.getByRole('button', { name: /cancel/i })
-
-	fireEvent.change(input, { target: { value: '' } })
-	expect(input.value).toBe('')
-
-	expect(saveButton).toBeDisabled()
-
+	const input = screen.getByRole('textbox')
 	fireEvent.change(input, { target: { value: 'something' } })
-	expect(input.value).toBe('something')
 
-	fireEvent.click(cancelButton)
-
+	const saveButton = screen.getByRole('button', { name: /save/i })
 	fireEvent.click(saveButton)
+	waitFor(() => {
+		expect(screen.getByText('something')).toBeInTheDocument()
+	})
+})
 
-	const text = screen.getByRole('paragraph') as HTMLParagraphElement
+test('delete card', () => {
+	render(<AppMain />)
 
-	expect(text.textContent).toBe('something')
+	const cards = screen.getAllByRole('card')
+	expect(cards.length).toBeGreaterThan(0)
 
-	const flashCard = screen.getByText('something')
-
-	expect(flashCard).toBeInTheDocument()
+	const editIcon = screen.getAllByAltText('edit icon')[0]
 	fireEvent.click(editIcon)
 
 	const deleteIcon = screen.getByAltText('trash icon')
 	fireEvent.click(deleteIcon)
 
-	expect(flashCard).not.toBeInTheDocument()
-})
-test('should display flashCards correctly', () => {
-	const flashCards = [
-		{ front: 'front1', back: 'back1', _id: '1', key: '1' },
-		{ front: 'front2', back: 'back2', _id: '2', key: '2' },
-	]
-	render(
-		flashCards.map(flashCard => {
-			return <DisplayCard front={flashCard.front} back={flashCard.back} _id={flashCard._id} key={flashCard.key} />
-		})
-	)
-	const firstFlashCard = screen.getByText('front1')
-	const secondFlashCard = screen.getByText('front2')
-
-	expect(firstFlashCard).toBeInTheDocument()
-	expect(secondFlashCard).toBeInTheDocument()
+	waitFor(() => {
+		expect(cards.length).toBe(cards.length - 1)
+	})
 })
